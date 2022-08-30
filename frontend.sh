33f62767d4a3c69efebb5c12e4cf891736dce586
Qwerci <theowerci@gmail.com>
2022-08-30 13:00:24 +0000

    simulating frontend failure

--- frontend.sh
+++ frontend.sh
@@ -4,7 +4,7 @@ function a(){
     echo ${URL}
     if curl -s ${URL} | grep "Welcome"; then
         #Change this to 0 after the job fails
-        return 0
+        return 1
     else
         return 1
     fi
